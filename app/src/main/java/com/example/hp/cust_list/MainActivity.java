b37005172404ef4c5f2f207e112a31837d90b315
hitanshi29 <hitanshi2996@gmail.com>
2017-05-11 15:39:04 +0530

    gridview

--- app/src/main/java/com/example/hp/cust_list/MainActivity.java
+++ app/src/main/java/com/example/hp/cust_list/MainActivity.java
@@ -4,16 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-    ListView l;
+    GridView l;
     int[] helly;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        l=(ListView)findViewById(R.id.l1);
+        l=(GridView) findViewById(R.id.l1);
         helly=new int[]{R.drawable.gujarat,R.drawable.rajasthan,R.drawable.mumbai,R.drawable.chennai,R.drawable.delhi,R.drawable.hydrabad,R.drawable.punjab,R.drawable.banglore,R.drawable.pune,R.drawable.kolkata};
 
         String[] st=getResources().getStringArray(R.array.state);
